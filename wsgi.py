e6bb51e02cbeb37230712f23fed3106eefe37c15
Rolando <rolando@example.com>
2026-07-15 19:51:57 -0500

    Simplify wsgi.py to import app from run

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from app import create_app
+# Importar directamente de run.py
+from run import app
 
-config_name = os.getenv('FLASK_ENV', 'production')
-app = create_app(config_name)
+if __name__ == "__main__":
+    app.run()
